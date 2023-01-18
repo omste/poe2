08ffc7a6cd069156cd0f7665d21bc1aafb00c87c
unknown <omrisuleiman15@gmail.com>
2023-01-18 08:16:13 +0100

    removed husky test code

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
-
 const App = (): JSX.Element => {
-  var x = 1
-  
   return (
     <div className="App">
       <header className="App-header">
