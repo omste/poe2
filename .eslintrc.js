83daaad847ea2e185cb820a46e60b922f87b934f
unknown <omrisuleiman15@gmail.com>
2023-01-18 07:02:06 +0100

    linting passing

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: 'poe2/tsconfig.json'
+    project: 'tsconfig.json'
   },
   plugins: ['react'],
   rules: {
