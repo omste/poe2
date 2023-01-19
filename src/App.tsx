06292b177d6516133f40fa61d778130ac5bf0df0
unknown <omrisuleiman15@gmail.com>
2023-01-19 02:44:52 +0100

    progress checkpoint

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import './App.css';
 import { ApolloProvider } from '@apollo/client';
 import { client } from './utils/apolloClient';
-import { RepoTable } from './components/organisms/repoTable';
+import { RepoPage } from './components/organisms/repoTable';
 
 const App = (): JSX.Element => {
   return (
     <ApolloProvider client={client}>
-      <RepoTable repoQuery={'react'} resNo={5} />
+      <RepoPage />
     </ApolloProvider>
   );
 };
--- src/components/organisms/repoTable.tsx
+++ src/components/organisms/repoTable.tsx
@@ -1,22 +1,59 @@
-import { useQuery } from '@apollo/client';
+import { useLazyQuery } from '@apollo/client';
+import { useState, useEffect } from 'react';
 import { REPO_SEARCH } from '../../queries/queries';
+import { QueryResult, EdgesEntity } from '../../types/repoQuery';
 
-export interface RepoTableProps {
-  repoQuery: string;
-  resNo: number;
+export enum SortColumns {
+  Name,
+  Stars,
+  Forks
 }
 
-export const RepoTable = ({ repoQuery, resNo }: RepoTableProps): JSX.Element => {
-  const { loading, error, data } = useQuery(REPO_SEARCH, {
-    variables: { repoQuery, resNo }
+export interface RepoTableProps {
+  resultSet: EdgesEntity[] | null | undefined;
+}
+
+export const RepoTable = ({ resultSet }: RepoTableProps): JSX.Element => {
+  console.log(resultSet);
+  return <div>hello table</div>;
+};
+
+export const RepoPage = (): JSX.Element => {
+  const repoQuery = 'react';
+  const resNo = 5;
+  const [getSearchTerm, setSearchTerm] = useState<string>(repoQuery);
+  const [getData, { loading, error, data }] = useLazyQuery<QueryResult>(REPO_SEARCH, {
+    variables: { repoQuery: getSearchTerm, resNo }
   });
+
+  useEffect(() => {
+    // TO DO : debounce this
+    // TO DO : fix TS promise error
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    getData();
+  }, []);
+
   if (loading) return <div>loading...</div>;
   if (error !== undefined) {
-    console.log(error);
+    console.error(error);
     return <h1>Error found</h1>;
   }
   if (data !== undefined) {
     console.log(data);
   }
-  return <div>Hello om !</div>;
+
+  return (
+    <div>
+      Hello om !
+      <input
+        type="text"
+        value={getSearchTerm}
+        placeholder="search term"
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+        }}
+      />
+      <RepoTable resultSet={data?.search.edges} />
+    </div>
+  );
 };
